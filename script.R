a67b008e1603fa616ebeff4ee48a5b341c28222e
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-11 17:25:56 -0500

    Added more charts and titles to each one

--- script.R
+++ script.R
@@ -15,46 +15,53 @@ data.frame(prop.table(table(d2$institucion, d2$rangoedad),2))
 inst <- data.frame(table(d2$institucion))
 colnames(inst) <- c("Institucion","Casos")
 ggplot(data=inst, aes(x=Institucion, y=Casos, fill=Institucion)) +
-  geom_bar(stat="identity", width=0.7, color="white") + coord_flip()
+  geom_bar(stat="identity", width=0.7, color="white") + coord_flip() +
+  ggtitle("Por institucion")
 
 #Piechart por Tipo de colaborador
 t_colab <- data.frame(table(d2$tipo))
 colnames(t_colab) <- c("Tipo", "Casos")
 ggplot(t_colab, aes(x="", y=Casos, fill=Tipo)) +
   geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) + theme_void()
+  coord_polar("y", start=0) + theme_void() +
+  ggtitle("Por tipo de colaborador")
 
 #BarPlot rango de edad
 r_edad <- data.frame(table(d2$rangoedad))
 colnames(r_edad) <- c("Rango","Casos")
 ggplot(data=r_edad, aes(x=Rango, y=Casos, fill=Rango)) +
-  geom_bar(stat="identity", width=0.7, color="white") + coord_flip()
+  geom_bar(stat="identity", width=0.7, color="white") + coord_flip() +
+  ggtitle("Por rango de edad")
 
 #Piechart por genero
 genero <- data.frame(table(d2$genero))
 colnames(genero) <- c("Genero", "Casos")
 ggplot(genero, aes(x="", y=Casos, fill=Genero)) +
   geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) + theme_void()
+  coord_polar("y", start=0) + theme_void() +
+  ggtitle("Por genero")
 
 #BarPlot por diagnostico
 diag <- data.frame(table(d2$diagnostico))
 colnames(diag) <- c("Diagnostico", "Casos")
 ggplot(data=diag, aes(x=Diagnostico, y=Casos, fill=Diagnostico)) +
-  geom_bar(stat="identity", width=0.7, color="white") + coord_flip()
+  geom_bar(stat="identity", width=0.7, color="white") + coord_flip() +
+  ggtitle("Por diagnostico")
 
 #Piechart por alta medica
 alta <- data.frame(table(d2$alta))
 colnames(alta) <- c("Alta_Medica", "Casos")
 ggplot(alta, aes(x="", y=Casos, fill=Alta_Medica)) +
   geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) + theme_void()
+  coord_polar("y", start=0) + theme_void() +
+  ggtitle("Por alta medica")
 
 #BarPlot por semana de contagio
 semana <- data.frame(table(d2$semanaContagio))
 colnames(semana) <- c("Semana", "Casos")
 ggplot(data=semana, aes(x=Semana, y=Casos, fill=Semana)) +
-  geom_bar(stat="identity", width=0.7, color="white") + coord_flip()
+  geom_bar(stat="identity", width=0.7, color="white") + coord_flip() +
+  ggtitle("Por semana de contagio")
 
 
 #LinePlot por contagios diarios y acumulados
@@ -62,7 +69,27 @@ casos <- as.data.frame(read_excel("Seguimiento_a_casos.xlsm", sheet = "Contagios
 colnames(casos) <- c("Fecha", "Contagios", "Acumulados")
 ggplot(casos, aes(x=Fecha,)) +
     geom_line( aes(y=Contagios), color="#69b3a2", size=1, alpha=0.9, linetype=1) + 
-    geom_line( aes(y=Acumulados), color="#69b3a2", size=1, alpha=0.9, linetype=1)
+    geom_line( aes(y=Acumulados), color="#69b3a2", size=1, alpha=0.9, linetype=1) +
+  ggtitle("Contagios diarios contra campus")
 
+#BarPlot por estado
+estado <- data.frame(table(d2$estado))
+colnames(estado) <- c("Estado", "Casos")
+ggplot(data=estado, aes(x=Estado, y=Casos, fill=Estado)) +
+  geom_bar(stat="identity", width=0.7, color="white") + coord_flip() +
+  geom_text(aes(label = Casos), hjust = -0.2) +
+  ggtitle("Por estado")
 
+#BarPlot tipo de institucion que otorga el servicio contra el campus
+inst_camp <- data.frame(prop.table(table(d2$institucion, d2$campus),2))
+colnames(inst_camp) <- c("Institucion", "Campus", "Casos")
+ggplot(inst_camp, aes(fill=Institucion, y=Campus, x=Casos)) + 
+  geom_bar(position="stack", stat="identity") +
+  ggtitle("Tipo de institucion contra campus")
 
+#BarPlot tipo de empleado contra el campus
+emp_camp <- data.frame(prop.table(table(d2$tipo, d2$campus),2))
+colnames(emp_camp) <- c("Empleado", "Campus", "Casos")
+ggplot(emp_camp, aes(fill=Empleado, y=Campus, x=Casos)) + 
+  geom_bar(position="stack", stat="identity") +
+  ggtitle("Tipo de empleado contra campus")
